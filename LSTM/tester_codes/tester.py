f1ce072203deb384636d539335803ab27e1bb9d9
PaulAllan209 <dpaulallan15@gmail.com>
2022-10-12 18:59:42 +0800

    pseudo coding for the multiple models plan

--- LSTM/tester_codes/tester.py
+++ LSTM/tester_codes/tester.py
@@ -1,8 +1,10 @@
-def prime_num():
-    # Return first value 
-    yield 2 
-    cache_prime = [2]
-    
-    
-    while True:
+location_list1 = ['Calamba', 'Cabuyao', 'Los banos', 'San pablo']
+
+def return_dict_of_locations(location_list):
+    returned_dict = {}
+    for loc in location_list:
+        returned_dict[loc] = location_list.index(loc)
         
+    return returned_dict
+        
+print(return_dict_of_locations(location_list1))
